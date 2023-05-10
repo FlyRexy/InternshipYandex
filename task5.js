f01f20bd7d6e9090624525eaff594cf7e3e69a24
FlyRexy <eferovnik@mail.ru>
2023-05-10 18:19:08 +0300

    promise task 5 corrected

--- task5.js
+++ task5.js
@@ -2,35 +2,26 @@ const solution = async function (request, showSpinner, hideSpinner) {
     const startTime = Date.now();
     let showSpinnerTimeoutId = null;
 
-    // запускаем запрос и показываем спиннер, если запрос выполняется дольше, чем 250 мс
-    const promise = request()
-        .then((response) => {
-            // вычисляем время выполнения запроса
+    request()
+        .then(() => {
             const endTime = Date.now();
-            const elapsedTime = endTime - startTime;
-
-            // отменяем таймаут показа спиннера, если он не был запущен
             if (showSpinnerTimeoutId !== null) {
                 clearTimeout(showSpinnerTimeoutId);
+                if (endTime - startTime > 250)
+                    hideSpinner()
             }
-
-            hideSpinner()
         });
 
-    // показываем спиннер, если запрос выполняется дольше, чем 250 мс
     showSpinnerTimeoutId = setTimeout(() => {
         showSpinner();
     }, 250);
-
-    // возвращаем промис, который резолвится после окончания выполнения запроса
-    return promise;
 
 }
 req = () => new Promise((resolve) => {
     setTimeout(() => {
         console.log('resolve')
         resolve();
-    }, 100);
+    }, 300);
 });
 const sh = function () {
     console.log("show spinner")
